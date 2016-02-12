a749d365934bddcb74e0e77d89d868b1cbe4b26d
Kristjan Heinaste <kristjan@ontapgroup.com>
2016-02-12 14:39:40 +0200

    Moved price calculation into backend, but still, same floating point precision problem happens

--- app/code/OnTap/Tns/Gateway/Request/Hosted/OrderDataBuilder.php
+++ app/code/OnTap/Tns/Gateway/Request/Hosted/OrderDataBuilder.php
@@ -10,9 +10,43 @@ use Magento\Payment\Gateway\Request\BuilderInterface;
 use Magento\Payment\Gateway\Helper\SubjectReader;
 use Magento\Sales\Model\Order\Payment;
 use Magento\Payment\Gateway\Data\Quote\QuoteAdapter;
+use Magento\Checkout\Model\CartFactory;
+use Magento\Checkout\Model\Cart;
 
 class OrderDataBuilder implements BuilderInterface
 {
+    /**
+     * @var Cart
+     */
+    private $cart;
+
+    /**
+     * OrderDataBuilder constructor.
+     * @param CartFactory $cartFactory
+     */
+    public function __construct(CartFactory $cartFactory)
+    {
+        $this->cart = $cartFactory->create();
+    }
+
+    protected function getItemData()
+    {
+        $data = [];
+        /** @var \Magento\Quote\Model\Quote\Item $item */
+        foreach($this->cart->getItems() as $item) {
+            $data[] = [
+                'name' => $item->getName(),
+                'description' => $item->getDescription(),
+                'sku' => $item->getSku(),
+                'unitPrice' => $item->getBasePrice(),
+                'quantity' => (int) $item->getQty(),
+                'unitTaxAmount' => $item->getBaseTaxAmount() / (float) $item->getQty(),
+            ];
+        }
+
+        return $data;
+    }
+
     /**
      * Builds ENV request
      *
@@ -26,10 +60,20 @@ class OrderDataBuilder implements BuilderInterface
         /* @var QuoteAdapter $order */
         $order = $paymentDO->getOrder();
 
+        /** @var Payment $payment */
+        $payment = $paymentDO->getPayment();
+
+        /** @var \Magento\Quote\Model\Quote $quote */
+        $quote = $payment->getQuote();
+        $quote->collectTotals();
+
         return [
             'order' => [
+                'amount' => sprintf('%.2F', $quote->getGrandTotal()),
                 'currency' => $order->getCurrencyCode(),
-                'id' => $order->getOrderIncrementId()
+                'id' => $order->getOrderIncrementId(),
+                //'item' => $this->getItemData(),
+                //'shippingAndHandlingAmount' => $quote->getShippingAmount(),
             ]
         ];
     }
