8eb48db37acb43ff9c537a7a9e96cd6c935e1963
Kristjan Heinaste <kristjan@ontapgroup.com>
2016-03-17 10:47:59 +0200

    Refactored Webhook/Response controller, added generic logging

--- app/code/OnTap/Tns/Controller/Webhook/Response.php
+++ app/code/OnTap/Tns/Controller/Webhook/Response.php
@@ -24,6 +24,9 @@ use OnTap\Tns\Gateway\Response\PaymentHandler;
 class Response extends \Magento\Framework\App\Action\Action
 {
     const X_HEADER_SECRET = 'X-Notification-Secret';
+    const X_HEADER_ATTEMPT = 'X-Notification-Attempt';
+    const X_HEADER_ID = 'X-Notification-Id';
+    const LOG_TYPE = 'webhook';
 
     /**
      * @var RawFactory
@@ -61,6 +64,11 @@ class Response extends \Magento\Framework\App\Action\Action
     protected $configProviders;
 
     /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
      * Response constructor.
      * @param Context $context
      * @param RawFactory $rawFactory
@@ -69,6 +77,7 @@ class Response extends \Magento\Framework\App\Action\Action
      * @param SearchCriteriaBuilder $searchCriteriaBuilder
      * @param FilterBuilder $filterBuilder
      * @param LoggerFactory $loggerFactory
+     * @param \Psr\Log\LoggerInterface $logger
      * @param \OnTap\Tns\Gateway\Config\Config[] $configProviders
      */
     public function __construct(
@@ -79,6 +88,7 @@ class Response extends \Magento\Framework\App\Action\Action
         SearchCriteriaBuilder $searchCriteriaBuilder,
         FilterBuilder $filterBuilder,
         LoggerFactory $loggerFactory,
+        \Psr\Log\LoggerInterface $logger,
         array $configProviders = []
     ) {
         parent::__construct($context);
@@ -88,7 +98,80 @@ class Response extends \Magento\Framework\App\Action\Action
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         $this->filterBuilder = $filterBuilder;
         $this->loggerFactory = $loggerFactory;
+        $this->logger = $logger;
         $this->configProviders = $configProviders;
+    }
+
+    /**
+     * Fetch a order object by transaction ID
+     *
+     * @param string $txnId
+     * @return \Magento\Sales\Api\Data\OrderInterface
+     * @throws NoSuchEntityException
+     */
+    protected function getOrderByTransactionId($txnId)
+    {
+        $filters[] = $this->filterBuilder
+            ->setField('txn_id')
+            ->setValue($txnId)
+            ->create();
+
+        $searchCriteria = $this->searchCriteriaBuilder
+            ->addFilters($filters)
+            ->create();
+
+        $transactions = $this->transactionRepository
+            ->getList($searchCriteria)
+            ->getItems();
+
+        if (count($transactions) < 1 || count($transactions) > 1) {
+            throw new NoSuchEntityException(__("Could not find transaction"));
+        }
+
+        /* @var \Magento\Sales\Api\Data\TransactionInterface $txn */
+        $txn = array_values($transactions)[0];
+
+        return $this->orderRepository->get($txn->getOrderId());
+    }
+
+    /**
+     * @param \Magento\Sales\Api\Data\OrderInterface $order
+     * @param array $data
+     * @return void
+     */
+    protected function updateOrderPaymentDetails(\Magento\Sales\Api\Data\OrderInterface $order, array $data)
+    {
+        $payment = $order->getPayment();
+
+        PaymentHandler::importPaymentResponse($payment, $data);
+
+        /* @var \Magento\Sales\Model\Order $order */
+        $order->addStatusHistoryComment(sprintf(
+            __("Order updated by gateway [ID: %s]"),
+            $this->getRequest()->getHeader(static::X_HEADER_ID)
+        ));
+        $order->save();
+    }
+
+    /**
+     * @param string $message
+     * @param string $callable
+     * @return void
+     */
+    protected function logWebHookRequest($message, $callable = 'info')
+    {
+        /* @var \Magento\Framework\App\Request\Http $request */
+        $request = $this->getRequest();
+
+        $responseAttempt = $request->getHeader(static::X_HEADER_ATTEMPT);
+        $responseId = $request->getHeader(static::X_HEADER_ID);
+        $logData = [
+            'type' => static::LOG_TYPE,
+            'attempt' => $responseAttempt,
+            'id' => $responseId,
+        ];
+
+        call_user_func_array([$this->logger, $callable], [$message, $logData]);
     }
 
     /**
@@ -99,8 +182,6 @@ class Response extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-        // @todo: implement tns_webhook.log log for generic logging
-
         /* @var \Magento\Framework\App\Request\Http $request */
         $request = $this->getRequest();
 
@@ -108,62 +189,50 @@ class Response extends \Magento\Framework\App\Action\Action
 
         $data = \Zend_Json_Decoder::decode($request->getContent(), \Zend_Json::TYPE_ARRAY);
 
+        $responseSecret = $request->getHeader(static::X_HEADER_SECRET);
+        $responseAttempt = $request->getHeader(static::X_HEADER_ATTEMPT);
+        $responseId = $request->getHeader(static::X_HEADER_ID);
+
         try {
-            $responseSecret = $request->getHeader(static::X_HEADER_SECRET);
             if ($responseSecret === false) {
-                throw new \Exception(__("Not authorized"));
+                throw new \Exception(__("Authorization not provided"));
             }
 
             if (!isset($data['transaction']) || !isset($data['transaction']['id'])) {
                 throw new \Exception(__("Invalid data received"));
             }
 
-            $filters[] = $this->filterBuilder
-                ->setField('txn_id')
-                ->setValue($data['transaction']['id'])
-                ->create();
+            $order = $this->getOrderByTransactionId($data['transaction']['id']);
 
-            $searchCriteria = $this->searchCriteriaBuilder
-                ->addFilters($filters)
-                ->create();
-
-            $txns = $this->transactionRepository
-                ->getList($searchCriteria)
-                ->getItems();
-
-            if (count($txns) < 1 || count($txns) > 1) {
-                throw new NoSuchEntityException(__("Could not find transaction"));
-            }
-
-            /* @var \Magento\Sales\Api\Data\TransactionInterface $txn */
-            $txn = array_values($txns)[0];
-
-            $order = $this->orderRepository->get($txn->getOrderId());
-            $payment = $order->getPayment();
-
-            $config = $this->configProviders[$payment->getMethod()];
+            $config = $this->configProviders[$order->getPayment()->getMethod()];
 
             if ($config->getWebhookSecret() !== $responseSecret) {
                 throw new \Exception(__("Authorisation failed"));
             }
+
+            // Update payment and order
+            $this->updateOrderPaymentDetails($order, $data);
+
         } catch (\Exception $e) {
+            $errorMessage = sprintf(__("Mastercard TNS WebHook Exception: '%s'"), $e->getMessage());
+            $this->logWebHookRequest($errorMessage, 'critical');
             $page->setStatusHeader(400);
             return $page->setContents($e->getMessage());
         }
 
-        PaymentHandler::importPaymentResponse($payment, $data);
-
-        /* @var \Magento\Sales\Model\Order $order */
-        $order->addStatusHistoryComment(__("Order updated by gateway"));
-        $order->save();
-
         $logger = $this->loggerFactory->create(['config' => $config]);
         $log = [
-            'type' => 'webhook',
+            'type' => static::LOG_TYPE,
+            'attempt' => $responseAttempt,
+            'id' => $responseId,
             'data' => $data,
         ];
         $logger->debug($log);
 
+        $logMessage = __("Mastercard TNS WebHook Success");
+        $this->logWebHookRequest($logMessage, 'info');
+
+        $page->setStatusHeader(200);
         return $page->setContents('');
     }
 }
