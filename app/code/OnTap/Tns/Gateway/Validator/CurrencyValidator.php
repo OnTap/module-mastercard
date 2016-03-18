f31395fdc96f092651c4a5e44affa866da514c4d
Kristjan Heinaste <kristjan@ontapgroup.com>
2016-03-18 15:54:07 +0200

    Added Currency and Country validator, both can be configured in admin

---
+++ app/code/OnTap/Tns/Gateway/Validator/CurrencyValidator.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace OnTap\Tns\Gateway\Validator;
+
+use Magento\Payment\Gateway\Validator\AbstractValidator;
+use Magento\Payment\Gateway\Validator\ResultInterfaceFactory;
+use Magento\Payment\Gateway\Validator\ResultInterface;
+use Magento\Payment\Gateway\ConfigInterface;
+
+class CurrencyValidator extends AbstractValidator
+{
+    /**
+     * @var \Magento\Payment\Gateway\ConfigInterface
+     */
+    private $config;
+
+    /**
+     * @param ResultInterfaceFactory $resultFactory
+     * @param ConfigInterface $config
+     */
+    public function __construct(
+        ResultInterfaceFactory $resultFactory,
+        ConfigInterface $config
+    ) {
+        $this->config = $config;
+        parent::__construct($resultFactory);
+    }
+
+    /**
+     * Performs domain-related validation for business object
+     *
+     * @param array $validationSubject
+     * @return ResultInterface
+     */
+    public function validate(array $validationSubject)
+    {
+        $isValid = true;
+        $storeId = $validationSubject['storeId'];
+        $currency = $validationSubject['currency'];
+
+        if ($currency !== $this->config->getValue('currency', $storeId)) {
+            $isValid = false;
+        }
+
+        return $this->createResult($isValid);
+    }
+}
