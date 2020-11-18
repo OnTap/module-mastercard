a524ee2b9929411632583e34b827ab6657a635b3
Maksym Grom <maksym.grom@ontapgroup.com>
2020-11-18 12:36:08 +0200

    MPGS-414 added options source for 3ds version selection

---
+++ Model/Config/Source/TreeDSecureVersion.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright (c) 2016-2020 Mastercard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace OnTap\MasterCard\Model\Config\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+
+class TreeDSecureVersion implements OptionSourceInterface
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 1, 'label' => __('3DS')],
+            ['value' => 2, 'label' => __('EMV 3DS (3DS2)')]
+        ];
+    }
+
+    /**
+     * Get options in "key-value" format
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return [
+            1 => __('3DS'),
+            2 => __('EMV 3DS (3DS2)')
+        ];
+    }
+}
